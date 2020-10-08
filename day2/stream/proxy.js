cd481e227cc780be68f17cc86823a745decd40a0
Moses A. Samuel <mosamuel98@gmail.com>
2020-10-07 17:25:45 -0700

    practice with duplex streams

--- day2/stream/proxy.js
+++ day2/stream/proxy.js
@@ -1,4 +1,6 @@
-var net = require('net')
-net.createServer(function (stream) {
-  stream.pipe(net.connect(5000,'localhost')).pipe(stream)
-}).listen(5005)
+var net = require("net");
+net
+	.createServer(function (stream) {
+		stream.pipe(net.connect(5000, "localhost")).pipe(stream);
+	})
+	.listen(5005);
